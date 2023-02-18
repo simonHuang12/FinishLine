7bb17406b1061d696cb5be70cf52a3bf83a66aa8
simonHuang12 <simonh8@nycstudents.net>
2023-02-17 19:40:12 -0500

    done

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,53 @@
-package PACKAGE_NAME;public class Game {
+import java.util.Arrays;
+
+public class Game {
+    private int[] p1 = {1, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    private int[] p2 = {1, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+    private int p1pos = 1;
+    private int p2pos = 1;
+    public Game(){
+        System.out.println("Player 1's board");
+        System.out.println(Arrays.toString(p1));
+        System.out.println("Player 2's board");
+        System.out.println(Arrays.toString(p2));
+    }
+    public int[] getP1(){
+        return p1;
+    }
+    public int[] getP2(){
+        return p2;
+    }
+
+    public int roll() {
+        return (int) (Math.random() * 6) + 1;
+    }
+
+    public void movePos(int player, int dice1, int dice2) {
+        boolean matchPosOne1 = dice1 == p1pos+1 || dice2 == p1pos+1;
+        boolean matchPosTotal1 = dice1 + dice2 == p1pos+1;
+        boolean matchPosOne2 = dice1 == p2pos+1 || dice2 == p2pos+1;
+        boolean matchPosTotal2 = dice1 + dice2 == p2pos+1;
+        if (player == 1) {
+            if (matchPosOne1 || matchPosTotal1){
+                p1[p1pos-1] = 0;
+                p1[p1pos] = 1;
+                p1pos++;
+                System.out.println(Arrays.toString(p1));
+            }else {
+                System.out.println("Miss! Next turn...");
+                System.out.println(Arrays.toString(p1));
+            }
+        }else if (player == 2) {
+            if (matchPosOne2 || matchPosTotal2){
+                p2[p2pos-1] = 0;
+                p2[p2pos] = 1;
+                p2pos++;
+                System.out.println(Arrays.toString(p2));
+            }else {
+                System.out.println("Miss! Next turn...");
+                System.out.println(Arrays.toString(p2));
+            }
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        final String ANSI_RESET = "\u001B[0m";
+        final String ANSI_RED = "\u001B[31m";
+        final String ANSI_BLUE = "\u001B[34m";
+        Game game = new Game();
+        Scanner scanner = new Scanner(System.in);
+        int turn = 0;
+        boolean p1Win;
+        boolean p2Win;
+        while ((game.getP1()[game.getP1().length-1] != 1)||(game.getP2()[game.getP2().length-1] != 1)){
+            p1Win = (game.getP1()[game.getP1().length-1] == 1);
+            p2Win = (game.getP2()[game.getP2().length-1] == 1);
+            if (turn % 2 == 0){
+                System.out.println(ANSI_BLUE+"Player 1's turn"+ANSI_RESET);
+            }else {
+                System.out.println(ANSI_RED+"Player 2's turn"+ANSI_RESET);
+            }
+            String input = scanner.nextLine();
+            int dice1 = game.roll();
+            int dice2 = game.roll();
+            System.out.println("Dice 1: "+dice1+" Dice 2: "+dice2);
+            if (turn % 2 == 0){
+                game.movePos(1, dice1, dice2);
+            }
+            if (turn % 2 == 1){
+                game.movePos(2, dice1, dice2);
+            }
+            turn++;
+            if (p1Win){
+                System.out.println(ANSI_BLUE+"Player 1 wins!"+ANSI_RESET);
+                break;
+            }else if (p2Win){
+                System.out.println(ANSI_RED+"Player 2 wins!"+ANSI_RESET);
+                break;
+            }
+        }
+
     }
 }
